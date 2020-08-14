b46ac94477e44ce42aa486a7874da6fa3f95c48e
Diogo45 <diogohartmann45@gmail.com>
2020-08-14 18:59:18 -0300

    Removed Poles in Fast IK, causing issues with inverting fingers

--- Assets/Scripts/ConfigurableJointExtensions.cs
+++ Assets/Scripts/ConfigurableJointExtensions.cs
@@ -2,58 +2,68 @@
 
 public static class ConfigurableJointExtensions
 {
-    /// <summary>
-    /// Sets a joint's targetRotation to match a given local rotation.
-    /// The joint transform's local rotation must be cached on Start and passed into this method.
-    /// </summary>
-    public static void SetTargetRotationLocal(this ConfigurableJoint joint, Quaternion targetLocalRotation, Quaternion startLocalRotation)
-    {
-        if (joint.configuredInWorldSpace)
-        {
-            Debug.LogError("SetTargetRotationLocal should not be used with joints that are configured in world space. For world space joints, use SetTargetRotation.", joint);
-        }
-        SetTargetRotationInternal(joint, targetLocalRotation, startLocalRotation, Space.Self);
-    }
+	/// <summary>
+	/// Sets a joint's targetRotation to match a given local rotation.
+	/// The joint transform's local rotation must be cached on Start and passed into this method.
+	/// </summary>
+	public static void SetTargetRotationLocal(this ConfigurableJoint joint, Quaternion targetLocalRotation, Quaternion startLocalRotation)
+	{
+		if (joint.configuredInWorldSpace)
+		{
+			Debug.LogError("SetTargetRotationLocal should not be used with joints that are configured in world space. For world space joints, use SetTargetRotation.", joint);
+		}
+		SetTargetRotationInternal(joint, targetLocalRotation, startLocalRotation, Space.Self);
+	}
 
-    /// <summary>
-    /// Sets a joint's targetRotation to match a given world rotation.
-    /// The joint transform's world rotation must be cached on Start and passed into this method.
-    /// </summary>
-    public static void SetTargetRotation(this ConfigurableJoint joint, Quaternion targetWorldRotation, Quaternion startWorldRotation)
-    {
-        if (!joint.configuredInWorldSpace)
-        {
-            Debug.LogError("SetTargetRotation must be used with joints that are configured in world space. For local space joints, use SetTargetRotationLocal.", joint);
-        }
-        SetTargetRotationInternal(joint, targetWorldRotation, startWorldRotation, Space.World);
-    }
+	/// <summary>
+	/// Sets a joint's targetRotation to match a given world rotation.
+	/// The joint transform's world rotation must be cached on Start and passed into this method.
+	/// </summary>
+	public static void SetTargetRotation(this ConfigurableJoint joint, Quaternion targetWorldRotation, Quaternion startWorldRotation)
+	{
+		if (!joint.configuredInWorldSpace)
+		{
+			Debug.LogError("SetTargetRotation must be used with joints that are configured in world space. For local space joints, use SetTargetRotationLocal.", joint);
+		}
+		SetTargetRotationInternal(joint, targetWorldRotation, startWorldRotation, Space.World);
+	}
 
-    static void SetTargetRotationInternal(ConfigurableJoint joint, Quaternion targetRotation, Quaternion startRotation, Space space)
-    {
-        // Calculate the rotation expressed by the joint's axis and secondary axis
-        var right = joint.axis;
-        var forward = Vector3.Cross(joint.axis, joint.secondaryAxis).normalized;
-        var up = Vector3.Cross(forward, right).normalized;
-        Quaternion worldToJointSpace = Quaternion.LookRotation(forward, up);
+	static void SetTargetRotationInternal(ConfigurableJoint joint, Quaternion targetRotation, Quaternion startRotation, Space space)
+	{
+		// Calculate the rotation expressed by the joint's axis and secondary axis
+		var right = joint.axis;
+		var forward = Vector3.Cross(joint.axis, joint.secondaryAxis);
+		//var forward = Vector3.Cross(joint.secondaryAxis, joint.axis);
+		var up = Vector3.Cross(forward, right);
+		//var up = Vector3.Cross(right, forward);
+		Quaternion worldToJointSpace = Quaternion.LookRotation(forward, up);
 
-        // Transform into world space
-        Quaternion resultRotation = Quaternion.Inverse(worldToJointSpace);
+		
 
-        // Counter-rotate and apply the new local rotation.
-        // Joint space is the inverse of world space, so we need to invert our value
-        if (space == Space.World)
-        {
-            resultRotation *= startRotation * Quaternion.Inverse(targetRotation);
-        }
-        else
-        {
-            resultRotation *= Quaternion.Inverse(targetRotation) * startRotation;
-        }
+		// Transform into world space
+		Quaternion resultRotation = Quaternion.Inverse(worldToJointSpace);
+		//Quaternion resultRotation = worldToJointSpace;
 
-        // Transform back into joint space
-        resultRotation *= worldToJointSpace;
+		// Counter-rotate and apply the new local rotation.
+		// Joint space is the inverse of world space, so we need to invert our value
+		if (space == Space.World)
+		{
+			resultRotation *= startRotation * Quaternion.Inverse(targetRotation);
+		}
+		else
+		{
+			//resultRotation *= targetRotation * startRotation;
+			resultRotation *= Quaternion.Inverse(targetRotation) * startRotation;
+			//resultRotation *= startRotation * Quaternion.Inverse(targetRotation);
+			//var quat = -resultRotation.eulerAngles;
+			//quat.y = resultRotation.y;
+			//resultRotation = Quaternion.Euler(quat);
+		}
 
-        // Set target rotation to our newly calculated rotation
-        joint.targetRotation = resultRotation;
-    }
+		// Transform back into joint space
+		resultRotation *= worldToJointSpace;
+
+		// Set target rotation to our newly calculated rotation
+		joint.targetRotation = resultRotation;
+	}
 }
--- Assets/Scripts/MimicRotation.cs
+++ Assets/Scripts/MimicRotation.cs
@@ -12,6 +12,7 @@ public class MimicRotation : MonoBehaviour
     {
         myJoint = GetComponent<ConfigurableJoint>();
         initialRotation = myJoint.transform.localRotation;
+        //initialRotation = myJoint.transform.rotation;
     }
     void Update()
     {
@@ -30,7 +31,53 @@ public class MimicRotation : MonoBehaviour
         //    rot.z = target.localEulerAngles.z;
         //}
 
-        myJoint.SetTargetRotationLocal(Quaternion.Euler(target.localEulerAngles.x, target.localEulerAngles.y, target.localEulerAngles.z), initialRotation);        
+    
+        //Quaternion targetLocalRotation = Quaternion.Euler(target.localEulerAngles.x, target.localEulerAngles.y, target.localEulerAngles.z);
+        //Quaternion targetLocalRotation = Quaternion.Euler(target.transform.localEulerAngles);
+
+        //if (gameObject.name == "f_middle.01.R")
+        //{
+
+        //    Debug.Log("TARGET " + target.transform.localRotation.eulerAngles);
+
+        //    var right = myJoint.axis;
+        //    var forward = Vector3.Cross(myJoint.axis, myJoint.secondaryAxis).normalized;
+        //    var up = Vector3.Cross(forward, right).normalized;
+        //    Quaternion worldToJointSpace = Quaternion.LookRotation(forward, up);
+
+        //    // Transform into world space
+        //    Quaternion resultRotation = Quaternion.Inverse(worldToJointSpace);
+
+        //    Debug.Log("RESULT 1 " + resultRotation.eulerAngles);
+
+
+        //    // Counter-rotate and apply the new local rotation.
+        //    // Joint space is the inverse of world space, so we need to invert our value
+        //    if (false)
+        //    {
+        //        resultRotation *= initialRotation * Quaternion.Inverse(target.transform.localRotation);
+        //    }
+        //    else
+        //    {
+        //        resultRotation *= Quaternion.Inverse(target.transform.localRotation) * initialRotation;
+        //        Debug.Log("RESULT 2 " + resultRotation.eulerAngles);
+
+        //    }
+
+        //    // Transform back into joint space
+        //    resultRotation *= worldToJointSpace;
+        //    Debug.Log("WORLD SPACE " + resultRotation.eulerAngles);
+
+
+
+
+        //}
+
+
+
+
+        //myJoint.SetTargetRotation(Quaternion.Euler(target.eulerAngles.x, target.eulerAngles.y, target.eulerAngles.z), initialRotation);        
+        myJoint.SetTargetRotationLocal(target.transform.localRotation, initialRotation);        
         //myJoint.SetTargetRotationLocal(Quaternion.Euler(rot.x, rot.y, rot.z), initialRotation);
     }
 }
