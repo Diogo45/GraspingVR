382b32fad5606d17208e9e7b93f9bf6ac9f92853
Diogo45 <diogohartmann45@gmail.com>
2020-10-27 16:34:52 -0300

    Implemented data gathering for relative positions and distances

--- Assets/Scripts/ParametrizedAnimation.cs
+++ Assets/Scripts/ParametrizedAnimation.cs
@@ -13,6 +13,8 @@ public class ParametrizedAnimation : MonoBehaviour
         public GameObject extreme;
     }
 
+    #region FingerData
+
     public float timeF;
     public float timeC;
 
@@ -35,10 +37,23 @@ public class ParametrizedAnimation : MonoBehaviour
     private float[] flexSpeed;
     private float[] curlSpeed;
 
+    #endregion
+
+    #region AnimData
+
+    public float IndexFingerDistanceToObject;
+
+    public (Vector3, Vector3, Vector3)[] PhalanxDistanceToObject;
+
+
+    #endregion
 
     // Start is called before the first frame update
     void Start()
     {
+
+        PhalanxDistanceToObject = new (Vector3, Vector3, Vector3)[fingers.Count-1];
+
         #region Initialize Values 
         flexSpeed = new float[fingers.Count];
         curlSpeed = new float[fingers.Count];
@@ -109,16 +124,35 @@ public class ParametrizedAnimation : MonoBehaviour
         //0.7; 0.1f; 0.7; 0.7; 0.7
         //0.4, 0.7, 0.2, 0.6
 
+
+        #region Gather Data
+
+#if UNITY_EDITOR
+        for (int i = 0; i < PhalanxDistanceToObject.Length; i++)
+        {
+            Debug.DrawLine(fingers[i].transform.position, PhalanxDistanceToObject[i].Item1);
+            Debug.DrawLine(curlFingers[i].middle.transform.position, PhalanxDistanceToObject[i].Item2);
+            Debug.DrawLine(curlFingers[i].extreme.transform.position, PhalanxDistanceToObject[i].Item3);
+        }
+#endif
+
+
+        //float indexDistance = Vector3 fingers[0]
+
+#endregion
+
+
         #region Update Phalanx Rotations
 
         for (int i = 0; i < fingers.Count; i++)
         {
-            fingers[i].transform.localRotation = Quaternion.Lerp(OldFingers[i], maxFingers[i], animTime[i] * flexTime[i] * timeF);
-            if (i == 1)
-            {
-                //fingers[i].transform.localRotation = OldFingers[i] * Quaternion.Lerp(OldFingers[i], maxFingers[i], animTime[i] * flexTime[i] * timeF);
-                fingers[i].transform.localRotation = Quaternion.Lerp(OldFingers[i], maxFingers[i], animTime[i] * flexTime[i] * timeF);
-            }
+            //fingers[i].transform.localRotation = Quaternion.Lerp(OldFingers[i], maxFingers[i], animTime[i] * flexTime[i] * timeF);
+            fingers[i].transform.localRotation = Quaternion.Lerp(OldFingers[i], maxFingers[i], animTime[i] * IndexFingerDistanceToObject);
+            //if (i == 1)
+            //{
+            //    //fingers[i].transform.localRotation = OldFingers[i] * Quaternion.Lerp(OldFingers[i], maxFingers[i], animTime[i] * flexTime[i] * timeF);
+            //    fingers[i].transform.localRotation = Quaternion.Lerp(OldFingers[i], maxFingers[i], animTime[i] * flexTime[i] * timeF);
+            //}
             
         }
 
@@ -128,6 +162,9 @@ public class ParametrizedAnimation : MonoBehaviour
             curlFingers[i].middle.transform.localRotation = Quaternion.Lerp(OldCurlFingers[i].Item1, maxCurl[i], /*curlTime[i] **/ animTime[i] * curlTime[i] * timeC);
 
             curlFingers[i].extreme.transform.localRotation = Quaternion.Lerp(OldCurlFingers[i].Item2, maxCurl[i],/* curlTime[i] **/ animTime[i] * curlTime[i] * timeC);
+            //curlFingers[i].middle.transform.localRotation = Quaternion.Lerp(OldCurlFingers[i].Item1, maxCurl[i], /*curlTime[i] **/ animTime[i] * curlTime[i] * timeC);
+
+            //curlFingers[i].extreme.transform.localRotation = Quaternion.Lerp(OldCurlFingers[i].Item2, maxCurl[i],/* curlTime[i] **/ animTime[i] * curlTime[i] * timeC);
            
         }
 
@@ -141,6 +178,41 @@ public class ParametrizedAnimation : MonoBehaviour
 
         #endregion
     }
+
+    private void OnTriggerStay(Collider collision)
+    {
+        IndexFingerDistanceToObject = Vector3.Distance(fingers[0].transform.position, collision.gameObject.transform.position);
+
+        for (int i = 0; i < PhalanxDistanceToObject.Length; i++)
+        {
+
+            RaycastHit hit;
+            if (Physics.Raycast(fingers[i].transform.position, fingers[i].transform.forward, out hit, 10))
+            {
+                PhalanxDistanceToObject[i].Item1 = hit.point;
+
+            }
+
+            if (Physics.Raycast(curlFingers[i].middle.transform.position, curlFingers[i].middle.transform.forward, out hit, 10))
+            {
+                PhalanxDistanceToObject[i].Item2 = hit.point;
+
+
+            }
+
+            if (Physics.Raycast(curlFingers[i].extreme.transform.position, curlFingers[i].extreme.transform.forward, out hit, 10))
+            {
+                PhalanxDistanceToObject[i].Item3 = hit.point;
+
+
+            }
+
+
+
+        }
+
+    }
+
 
 
     private Quaternion quatCopy(Quaternion a)
